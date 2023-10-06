dc7e644effbfd353f426f09fc126e6ca674e313a
Bre <breanna.schmidt12@gmail.com>
2023-10-06 15:56:26 -0400

    added ids and names to form elements so it's functional with autocomplete. Still need to readjust image so it doesn't mess with form layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,29 +4,27 @@ function Home () {
 			<div id='tagdiv'>
 				<h2 id='tagline'>Where Little Hearts Grow, Learn, and Shine Brightly</h2>
 			</div>
-
-			 <img id="homeImg" src={require('../img/istockphoto-1397844973-170667a.webp')} alt='kids at school'></img>
-			
-      <form className='formsec'>
+		  <img id="homeImg" src={require('../img/istockphoto-1397844973-170667a.webp')} alt='kids at school'></img>
+      <form className='formsec' id='form'>
 			 <h2 className='title is-3'>Fill out this form to learn more!</h2>
 			 <div className='field'>
-				 <label className='label'>First Name</label>
-				 <div className='control' id='fname'>
-					 <input className='input' type='text' placeholder='First Name'/>
-				 </div>
-			 </div>
-
-			 <div className='field'>
-				 <label className='label'>Last Name</label>
+				 <label className='label' for="fname">First Name</label>
 				 <div className='control'>
-					 <input className='input' type='text' placeholder='Last Name'/>
+					 <input className='input' type='text' placeholder='First Name' id='fname'/>
 				 </div>
 			 </div>
 
 			 <div className='field'>
-				 <label className='label'>Email</label>
+				 <label className='label' for="lname">Last Name</label>
+				 <div className='control'>
+					 <input className='input' type='text' placeholder='Last Name' id='lname'/>
+				 </div>
+			 </div>
+
+			 <div className='field'>
+				 <label className='label' for="email">Email</label>
 				 <div className='control has-icons-left has-icons-right'>
-					 <input className='input' type='email' placeholder='Email'/>
+					 <input className='input' type='email' placeholder='Email' id='email'/>
 				   <span className='icon is-small is-left'>
 					   <i className='fas fa-envelope'></i>
 					  </span>
@@ -37,9 +35,9 @@ function Home () {
 			  </div>
 
 			 <div className='field'>
-				 <label className='label'>Message</label>
+				 <label className='label' for="message">Message</label>
 				 <div className='control'>
-					 <textarea className='textarea' placeholder='Message'></textarea>
+					 <textarea className='textarea' placeholder='Message' id='message'></textarea>
 				 </div>
 			 </div>
 
@@ -49,7 +47,6 @@ function Home () {
 			    </div>
 			 </div>
 			</form>
-
 		</div>
 	)
 }
