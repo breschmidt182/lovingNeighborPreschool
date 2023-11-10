ca77987997fa22399a3078777c6ed4224f80cb36
Bre <breanna.schmidt12@gmail.com>
2023-11-10 12:11:32 -0500

    trying to style homepage. Added short about us to home page. Need to restyle home page and logo.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,35 @@
 import 'bulma/css/bulma.min.css';
 const Navigation = (props) => {
 	return(
+	 <>
 		<nav id= 'nav' className='navbar is-spaced is-light has-shadow' role='navigation' aria-label= "main navigation">
 			<div className="navbar-brand scale-in-hor-left">
        <a className="navbar-item title is-4" id='navName' href="/">
-			 	 <img src={require('../img/logo.png')} alt='logo'></img> 
-			   Loving Neighbor Preschool
+			 	 <img src={require('../img/logo.png')} alt='logo' id='logo'></img> 
 	     </a>
       </div>
-			<div id='navbarBasicExample' className='nabar-menu has-dropdown is-active title is-4'>
-				<div className='navbar-end'>
-					<a className='navbar-item slide-in-right' id='navItem' href='/aboutus'>About Us</a>
-					<a className='navbar-item slide-in-right' id='navItem'  href='/staff'>Meet the Team</a>
-					<a className='navbar-item slide-in-right' id='navItem'  href='/calendar'>Calendar</a>
-					<a className='navbar-item slide-in-right' id='navItem' href='/contactus'>Contact Us</a>
-					<a className='navbar-item slide-in-right' id='navItem' href='/enrollment'>Enrollment</a>
-				</div>
+			<div id='navbarBasicExample' className='navbar-menu has-dropdown  title is-4'>
+				<ul className='navbar-end'>
+					<li>
+						<a className='navbar-item slide-in-right' id='navItem' href='/aboutus'>About Us</a>
+					</li>
+					<li>
+						<a className='navbar-item slide-in-right' id='navItem'  href='/staff'>Meet the Team</a>
+					</li>
+					<li>
+						<a className='navbar-item slide-in-right' id='navItem'  href='/calendar'>Calendar</a>
+					</li>
+					<li>
+						<a className='navbar-item slide-in-right' id='navItem' href='/contactus'>Contact Us</a>
+					</li>
+					<li>
+						<a className='navbar-item slide-in-right' id='navItem' href='/enrollment'>Enrollment</a>
+					</li>
+			  </ul>
 			</div>
 		</nav>
+
+	 </>
 	)
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
 function Home () {
 	return(
 		<div>
 			<div id='tagdiv'>
 				<h2 id='tagline'>Where Little Hearts Grow, Learn, and Shine Brightly</h2>
 			</div>
-		  <img id="homeImg" src={require('../img/istockphoto-1397844973-170667a.webp')} alt='kids at school'></img>
-      <form className='formsec' id='form'>
+		  <img className="homeImg" src={require('../img/istockphoto-1397844973-170667a.webp')} alt='kids at school'></img> 
+			 <div id='missiondiv'>
+				 <h2 id='mission'>At Loving Neighbors Preschool, we embrace a profound sense of purpose, echoing the values and vision of Northern Lakes Community Church. Our mission is to extend a hand of love and service to our community, addressing the pressing need for accessible childcare in the Grand Traverse area.
+					 Loving Neighbors Preschool is a testament to our commitment to making dreams a reality. With open hearts and open doors, we welcome families from all walks of life to join us on this remarkable journey of love, learning, and community-building. 
+					 Together, we embrace the future with hope, knowing that by loving our neighbors, we can make a lasting impact on the lives of our children and families.
+				 </h2>
+			 </div>
+      <form className='formsec form-horizontal' id='form'>
 			 <h2 className='title is-3'>Fill out this form to learn more!</h2>
 			 <div className='field'>
-				 <label className='label' for="fname">First Name</label>
+				 <label className='label control-label' for="fname">First Name</label>
 				 <div className='control'>
 					 <input className='input' type='text' placeholder='First Name' id='fname'/>
 				 </div>
 			 </div>
 
 			 <div className='field'>
-				 <label className='label' for="lname">Last Name</label>
+				 <label className='label control-label' for="lname">Last Name</label>
 				 <div className='control'>
 					 <input className='input' type='text' placeholder='Last Name' id='lname'/>
 				 </div>
 			 </div>
 
 			 <div className='field'>
-				 <label className='label' for="email">Email</label>
+				 <label className='label control-label' for="email">Email</label>
 				 <div className='control has-icons-left has-icons-right'>
 					 <input className='input' type='email' placeholder='Email' id='email'/>
 				   <span className='icon is-small is-left'>
@@ -35,7 +43,7 @@ function Home () {
 			  </div>
 
 			 <div className='field'>
-				 <label className='label' for="message">Message</label>
+				 <label className='label control-label' for="message">Message</label>
 				 <div className='control'>
 					 <textarea className='textarea' placeholder='Message' id='message'></textarea>
 				 </div>
